4f419d02fe3ebf1e098ea417b4a76763efe4e4a3
Jojolinul <lucianhuza@yahoo.com>
2017-11-22 15:07:57 +0000

    Triggered
    Deleted extra code.

--- Source/RoboHood/RProjectileBase.h
+++ Source/RoboHood/RProjectileBase.h
@@ -38,8 +38,6 @@ protected:
 	class USphereComponent* CollisionSphereComponent;
 
 	UFUNCTION()
-	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {}
-	UFUNCTION()
 	virtual void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit) {}
 
 public:
